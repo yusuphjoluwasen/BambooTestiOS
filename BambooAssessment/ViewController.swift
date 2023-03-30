5c26df68301805ab7f894095932d902fe952a80f
Guru <yusuphjoluwaseun@gmail.com>
2023-03-30 05:14:54 +0100

    Initial Commit

---
+++ BambooAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BambooAssessment
+//
+//  Created by Guru King on 30/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
